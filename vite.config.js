1d7275c455f8b204f2196d30b8e52dc2d823ccb9
qqotrunnada220526 <qqotrunnada220526@gmail.com>
2025-09-07 15:33:46 +0000

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Vite will automatically expose environment variables prefixed with VITE_
   // to import.meta.env.
+  base: './',
 });
